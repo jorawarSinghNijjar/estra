fadbdbb81c77183daa6a36ced507f301f7849f1e
Jorawar Singh <jorawarsinghnijjar@gmail.com>
2022-12-28 13:10:11 -0500

    complete structure

---
+++ pages/api/stripe.js
@@ -0,0 +1,59 @@
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    console.log(req.body);
+    const cartItems = req.body;
+    try {
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1MJsKeERHUpWMJIt0ZvEKWRr" },
+          { shipping_rate: "shr_1MJsLEERHUpWMJIthLgCbXdy" },
+        ],
+        line_items: cartItems.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/m9enmbio/production/"
+            )
+            .replace("-png", ".png")
+            .replace("-jpg", ".jpg");
+
+          console.log("newImage------", newImage);
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+                enabled: true,
+                minimum: 1
+            },
+            quantity: item.quantity
+          };
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+      };
+
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
---
+++ pages/success.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useStateContext } from "../context/StateContext";
+
+import { BsBagCheckFill } from "react-icons/bs";
+import Link from "next/link";
+import { fireAll } from "./../lib/utils";
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+  const [order, setOrder] = useState(null);
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+
+    fireAll();
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order !</h2>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:orders@estra.com">
+            orders@estra.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" className="btn" width="300px">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
